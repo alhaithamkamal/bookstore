8c95c68d0f3820188cbb9c02b09d1993c3c54d89
alhaithamkamal <alhaithamkamalali@gmail.com>
2022-08-19 01:37:59 +0200

    product image and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ApplicationRecord
+    has_one_attached :image
+    validates :name, :price, :description, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validate :acceptable_image
+
+    def acceptable_image
+        return unless image.attached?
+      
+        unless image.byte_size <= 2.megabyte
+          errors.add(:image, "is too big")
+        end
+
+        acceptable_types = ["image/jpeg", "image/png"]
+        unless acceptable_types.include?(image.content_type)
+            errors.add(:image, "must be a JPEG or PNG")
+        end
+    end
 end
