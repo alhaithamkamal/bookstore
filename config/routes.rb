bc09594fb64de383380246bfb84ed47d57ff5dfe
alhaithamkamal <alhaithamkamalali@gmail.com>
2022-08-19 18:06:25 +0200

    admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'products#index'
+  resources :admins, :only => [:show]
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'sign_in', to: 'sessions#new'
