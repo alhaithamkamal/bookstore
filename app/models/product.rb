976cf063720e733c0362dd385400d8d7c0a50d8a
alhaithamkamal <alhaithamkamalali@gmail.com>
2022-08-19 18:25:22 +0200

    final edits

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
     has_one_attached :image
     belongs_to :admin, optional: true
-    validates :name, :price, :description, presence: true
+    validates :name, :price, :description, :image, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 0 }
     validate :acceptable_image
 
